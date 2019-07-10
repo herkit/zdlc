8e49f439efa4f4c71fd825e38672867403c70b49
Henrik Grotle <henrik.grotle@arasoft.no>
2019-07-10 20:05:11 +0200

    working on layout

--- src/ZDLoanCalculator.Web/ClientApp/src/app/app.module.ts
+++ src/ZDLoanCalculator.Web/ClientApp/src/app/app.module.ts
@@ -3,14 +3,51 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
+import { LoanApplicationComponent } from './loan-application/loan-application.component';
+
+import {
+  MatStepperModule,
+  MatRadioModule,
+  MatInputModule,
+  MatButtonModule,
+  MatSliderModule
+} from '@angular/material';
+
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+const FRAMEWORK_MODULES = [
+  FormsModule,
+  ReactiveFormsModule,
+  BrowserAnimationsModule
+]
+
+const MATERIAL_MODULES = [
+  MatStepperModule,
+  MatRadioModule,
+  MatInputModule,
+  MatButtonModule,
+  MatSliderModule
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoanApplicationComponent
   ],
   imports: [
+    ...FRAMEWORK_MODULES,
+    ...MATERIAL_MODULES,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'apply', component: LoanApplicationComponent }
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/ZDLoanCalculator.Web/ClientApp/src/app/loan-application/loan-application.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loan-application',
+  templateUrl: './loan-application.component.html',
+  styleUrls: ['./loan-application.component.scss']
+})
+export class LoanApplicationComponent implements OnInit {
+
+  constructor() { }
+
+  loanTypes: any[] = [
+    { key: "house", description: "House loan", interestRate: 0.035 },
+    { key: "car", description: "Car loan", interestRate: 0.035 },
+  ];
+
+  paymentSchemes: any[] = [
+    { key: "series", value: "Series loan" },
+    { key: "annuity", value: "Annuity loan" },
+  ]
+
+  ngOnInit() {
+  }
+
+}
